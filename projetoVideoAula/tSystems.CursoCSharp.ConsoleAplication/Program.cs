61b8ee50bada582c4bcf699244ff442fc40a0a61
Victor Silva de Souza <fe605@t-systems.com.br>
2022-01-24 17:55:02 -0300

    [ADD] Documentos e imagens

---
+++ projetoVideoAula/tSystems.CursoCSharp.ConsoleAplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tSystems.CursoCSharp.ConsoleAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomeCorrentista = "";
+            nomeCorrentista = Console.ReadLine();
+            Console.Write("Nome da variável = "+ nomeCorrentista);
+            Console.ReadLine();
+        }
+    }
+}
